8d6ae7fc7ee111b505211995d3221d69d841382e
jessica <jessica.mcinchak@gmail.com>
2018-03-14 10:39:57 -0400

    fetch data for single issue type

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ class App extends Component {
     this.state = {
       defaultWeeks: 3,
       defaultIssueType: '*',
+      startDate: '',
+      endDate: '',
       data: [],
       fetchedData: false,
     }
@@ -26,7 +28,8 @@ class App extends Component {
 
   handleDateChange(value) {
     if (value) {
-      this.fetchIssues(value);
+      console.log('changed date');
+      this.fetchAllIssueTypes(value);
       this.setState({ defaultWeeks: value });
     }
   }
@@ -38,15 +41,22 @@ class App extends Component {
     }
   }
 
-  fetchIssues(week) {
-    let start = moment().subtract(week, 'week').format('YYYY-MM-DD');
+  /**
+   * Query Improve Detroit for all issue types submitted during a certain timeframe and get summary counts
+   * @param {int} numWeeks 
+   * @returns {promise}
+   */
+  fetchAllIssueTypes(numWeeks) {
+    let start = moment().subtract(numWeeks, 'week').format('YYYY-MM-DD');
     let end = moment().format('YYYY-MM-DD');
-    console.log(`Fetching data for last ${week} weeks:`, start, '-', end);
+    console.log(`Fetching data for last ${numWeeks} weeks:`, start, '-', end);
 
     fetch(`https://data.detroitmi.gov/resource/a9kb-mhiu.json?$query=SELECT request_type_title, COUNT(created_at) AS created_count, COUNT(closed_at) AS closed_count, AVG(days_to_close) AS avg_days_to_close, COUNT(reopened_at) as reopened_count WHERE created_at between '${start}' and '${end}' GROUP BY request_type_title`)
     .then(res => res.json())
     .then(d => {
       this.setState({
+        startDate: start,
+        endDate: end,
         data: _.sortBy(d, d => d.request_type_title),
         fetchedData: true,
       });
@@ -55,12 +65,12 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.fetchIssues(this.state.defaultWeeks);
+    this.fetchAllIssueTypes(this.state.defaultWeeks);
   }
 
   render() {
     return (
-      <div style={{ margin: '1.5em', display: 'flex', flexDirection: 'column', }}>
+      <div style={{ margin: '1.5em', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <h1>Analyze Improve Detroit Issues...</h1>
         { this.state.fetchedData ? 
           <div>
@@ -68,9 +78,7 @@ class App extends Component {
               <IssuePicker issues={this.state.data} onChange={this.handleIssueTypeChange} />
               <DatePicker weeks={this.state.defaultWeeks} onChange={this.handleDateChange} />
             </div>
-            <div>
-              { this.state.defaultIssueType === '*' ? <AllIssuesSummary issues={this.state.data} /> : <IssueSummary type={this.state.defaultIssueType} /> }
-            </div>
+            { this.state.defaultIssueType === '*' ? <AllIssuesSummary issues={this.state.data} /> : <IssueSummary type={this.state.defaultIssueType} start={this.state.startDate} end={this.state.endDate} /> }
           </div>
         : <p>Loading...</p> }
       </div>
--- src/components/IssueSummary.js
+++ src/components/IssueSummary.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class IssueSummary extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       data: [],
@@ -10,18 +10,40 @@ class IssueSummary extends Component {
     }
   }
 
-  fetchData() {
-    console.log(this.props.type)
+  /**
+   * Query Improve Detroit for one type of issue submitted during a certain timeframe and get all ticket fields
+   * @param {string} type - request type title
+   * @param {string} start - start date YYYY-MM-DD
+   * @param {string} end  - end date YYYY-MM-DD
+   * @returns {promise}
+   */
+  fetchIssuesByType(type, start, end) {
+    fetch(`https://data.detroitmi.gov/resource/a9kb-mhiu.json?$limit=50000&$where=request_type_title = '${type}' AND created_at between '${start}' and '${end}'`)
+    .then(res => res.json())
+    .then(d => {
+      this.setState({
+        data: d,
+        fetchedData: true,
+      });
+    })
+    .catch(e => console.log(e));
   }
 
   componentDidMount() {
-    this.fetchData()
+    this.fetchIssuesByType(this.props.type, this.props.start, this.props.end);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.type !== nextProps.type || this.props.start !== nextProps.start) {
+      this.fetchIssuesByType(nextProps.type, nextProps.start, nextProps.end);
+    }
   }
 
   render() {
     return (
       <div>
         <h3>{this.props.type}</h3>
+        { this.state.fetchedData ? <p>{`${this.state.data.length} tickets created`}</p> : <p>Loading...</p> }
       </div>
     );
   }
