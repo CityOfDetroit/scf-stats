1a3e60a713bfeba696890e8eca786c530b2ed525
jessica <jessica.mcinchak@gmail.com>
2018-12-17 09:06:21 -0500

    comma formatting

--- src/components/AllIssuesTable.js
+++ src/components/AllIssuesTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import numeral from 'numeral';
 
 import Helpers from '../helpers';
 
@@ -32,9 +33,9 @@ class AllIssuesTable extends Component {
                 <TableCell numeric>{Helpers.slas[i.request_type_title] > 0 ? `${Helpers.slas[i.request_type_title]} days` : null}</TableCell>
                 <TableCell style={{ fontWeight: 700 }} numeric>{i.closed_within_sla > 0 && i.closed_count > 0 ? `${_.round((i.closed_within_sla/i.closed_count)*100, 1)}%` : null}</TableCell>
                 <TableCell style={{ borderRight: '1px solid #eee' }} numeric>{i.avg_days_to_close > 0 ? `${_.round(i.avg_days_to_close, 1)} days` : null}</TableCell>
-                <TableCell numeric>{i.created_count}</TableCell>
-                <TableCell numeric>{i.closed_count}</TableCell>
-                <TableCell numeric>{i.reopened_count}</TableCell>
+                <TableCell numeric>{numeral(i.created_count).format('0,0')}</TableCell>
+                <TableCell numeric>{numeral(i.closed_count).format('0,0')}</TableCell>
+                <TableCell numeric>{numeral(i.reopened_count).format('0,0')}</TableCell>
               </TableRow>
             );
           })}
